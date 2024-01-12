e052c133f959c70234c76a2f37659f2dbb711cb7
itsmariush <itsmariush@users.noreply.github.com>
2024-01-12 14:20:13 +0100

    first working version

--- crc.py
+++ crc.py
@@ -1,48 +1,57 @@
-import BitVector as bv
+from BitVector import BitVector
+import binascii
 
 def reverse_bits(byte):
     binary_representation = bin(byte)[2:].zfill(8)
     reversed_binary = int(binary_representation[::-1], 2)
-    return reversed_binary.to_bytes(1, byteorder='big')
+    return reversed_binary
 
-def reverse_bytes_in_string(data):
-    reversed_data = [reverse_bits(byte) for byte in data]
-    return reversed_data
+def msb_to_lsb(m):
+    b1 = m >> 24 & 0xff
+    b2 = m >> 16 & 0xff
+    b3 = m >> 8 & 0xff
+    b4 = m >> 0 & 0xff
+    r1 = reverse_bits(b1)
+    r2 = reverse_bits(b2)
+    r3 = reverse_bits(b3)
+    r4 = reverse_bits(b4)
+    r = 0 | r1
+    r = r << 8 | r2
+    r = r << 8 | r3
+    r = r << 8 | r4
+    return r
 
-# Example usage:
-original_byte = b'\xBA'  # Example byte
-reversed_byte = reverse_bits(original_byte[0])
+# https://www.reddit.com/r/crypto/comments/3h6qeh/generating_crc32_collisions/
+def crc32_collision(msg):
+    # CRC32 polynomial
+    poly = BitVector(intVal = 0x104C11DB7)
+    print("CRC32 Generator polynomial:", hex(poly.int_val()))
 
-print("Original byte:", original_byte)
-print("Reversed byte:", reversed_byte)
-# CRC32 polynomial
-poly = bv.BitVector(intVal = 0x104C11DB7)
+    # multiplicative inverse of x^32, or 0x100000000
+    inv = BitVector(intVal = 0x100000000).gf_MI(poly, 32)
+    print("Multiplicative inverse for x^32 in mod polynomial:", hex(inv.int_val()))
 
-# multiplicative inverse of x^32, or 0x100000000
-inv = bv.BitVector(intVal = 0x100000000).gf_MI(poly, 32)
-print(inv.getHexStringFromBitVector()) # output cbf1acda
+    m1 = msg
+    m1_crc = binascii.crc32(m1.to_bytes((m1.bit_length() + 7) // 8, byteorder='big'))
+    m2 = m1 << 32
+    m2_crc = binascii.crc32(m2.to_bytes((m2.bit_length() + 7) // 8, byteorder='big'))
 
-m1 = 0xF4EF771F # 0xF4EF771F
-m2 = 0x1BB63F26 # = CRC(0x818080810f00000000) #0x597A4F4E=CRC(0x123456789A00000000)
+    print("m1: {} m2: {}".format(hex(m1), hex(m2)))
+    print("m1_crc: {} m2_crc: {}".format(hex(m1_crc), hex(m2_crc)))
 
-xor = int('{:08b}'.format(m1 ^ m2)[::-1], 2)
-print(xor)
+    xor = m1_crc ^ m2_crc
+    k = int(bin(xor)[2:][::-1], 2)
 
-k = xor
-# k*inv mod poly
-p = bv.BitVector(intVal = k).gf_multiply_modular(inv, poly, 32)
-print(p.get_bitvector_in_hex())
+    # k*inv mod poly
+    p = BitVector(intVal = k).gf_multiply_modular(inv, poly, 32)
+    res = p.int_val()
+    print("m1_crc XOR m2_crc * x^32_inverse in modulo polynomial =", hex(res))
 
-h = 0x25d5cbcd #0x5d9fe0f5 #0xdcd1ea47
-byte_array = h.to_bytes(4, byteorder='big')
-rev = reverse_bytes_in_string(byte_array)
-print(rev)
-hex_string = ''.join([byte.decode('utf-8') if isinstance(byte, bytes) else byte for byte in rev])
-print(hex_string) # = 0x3b8b57e2
-# p = 0011 0110 1110 1010 0011 0001 0011 0000
+    res_lsb = msb_to_lsb(res)
+    print("with Least Significant Bit first:", hex(res_lsb))
+    col = m2 ^ res_lsb
+    col_crc = binascii.crc32(col.to_bytes((col.bit_length() + 7) // 8, byteorder='big'))
+    print("m2:", hex(col), " CRC(m2) =", hex(col_crc))
 
-# 0x818080810fa4abd3b3 !!!!!!
-
-# 0101 1101 1001 1111 1110 0000 1111 0101
-# 1011 1010   
-# 1011 1010 1111 1001 0000 0111 1010 1111
+if __name__ == "__main__":
+    crc32_collision(0x818080810f)
